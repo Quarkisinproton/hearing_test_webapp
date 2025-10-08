4d1ec4aaff9d3e797c3af8113164339064fc83ae
Quarkisinproton <quarkisinproton@gmail.com>
2025-10-08 22:57:21 +0530

    PWA part 7

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,8 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#e6f2fb" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
